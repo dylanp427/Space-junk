28f12ae71bc8deaa9696c750ec04976a3b3182e7
dylanp <46984702+dylanp427@users.noreply.github.com>
2026-05-16 21:40:39 +1000

    Add files via upload

---
+++ datavisualization2.js
@@ -0,0 +1,24 @@
+var vg_1 = "Chart1.json";
+vegaEmbed("#c", vg_1).then(function(result) {
+
+}).catch(console.error);
+var vg_2 = "Chart2.json";
+vegaEmbed("#c2", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "Chart3.json";
+vegaEmbed("#c3", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "Chart4.json";
+vegaEmbed("#c4", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_5 = "Chart5.json";
+vegaEmbed("#c5", vg_5).then(function(result) {
+
+}).catch(console.error);
